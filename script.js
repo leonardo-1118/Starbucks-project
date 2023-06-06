cb352c53c75e82e509760cb1239a8e6b4ab1f8ef
Leonardo <moreiraleo700@gmail.com>
2023-06-06 14:03:54 -0300

    Separando Script

---
+++ script.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".green")
+let circle = document.querySelector(".circle")
+
+function changeimg(endereco) {
+    img.src = endereco
+}
+
+function changeColor(color) {
+    circle.style.background = color
+}
